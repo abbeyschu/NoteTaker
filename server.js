83d5af1ca857a4d69c7b076c35e1003f625ada57
Abbey Schueller <schuellerabbey@gmail.com>
2021-04-20 19:18:02 -0700

    updated API routes

--- server.js
+++ server.js
@@ -12,26 +12,17 @@ const PORT = process.env.PORT||3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static("public"));
 
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
-// => HTML GET Requests  
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'./public/index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'./public/notes.html')));
-    
-// If no matching route is found default to home
-app.get('*', (req, res) => res.sendFile(path.join(__dirname,'./public/index.html')));
-
-
-    // API GET Requests  
-const notes = require(__dirname + '/db/db.json');
+// API GET Requests  
+const notes = require('./db/db.json');
 app.get('/api/notes', (req, res) => res.json(notes));
-  
-    // API POST Requests
+   
+// API POST Requests
 app.post('/api/notes', (req, res) => {
     notes.push(req.body);
     let id = 1;
@@ -40,8 +31,32 @@ app.post('/api/notes', (req, res) => {
         id++;
         return notes;
     });
+    res.json(true);
     fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
         if (err) throw err;
     });
     res.end();
+});
+
+app.delete('/api/notes/:id', function(req, res) {
+    const deleteNote = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === Number(deleteNote)) {
+          notes.splice(i,1);
+        };
+    fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+        if (err) throw err;
+      });
+    res.end();
+}});
+
+
+// HTML GET Requests  
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
+});
+    
+// If no matching route is found default to home
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'))
 });
